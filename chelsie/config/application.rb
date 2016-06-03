2a356de8f393055bb77099efe4fac6a384be32be
Elizabeth Brown <email": null>
2016-06-03 15:02:20 -0700

    Attempt to get .gitignore to work on .env file

--- chelsie/config/application.rb
+++ chelsie/config/application.rb
@@ -6,9 +6,26 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+require "rails"
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+
 module Chelsie
   class Application < Rails::Application
     config.api_only = true
+    config.middleware.insert_before 'Rack::Runtime', 'Rack::Cors' do
+      allow do
+        origins '*' #localhost:9000
+        resource '*',
+                 headers: :any,
+                 methods: [:get, :put, :post, :patch, :delete, :options]
+      end
+  end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
