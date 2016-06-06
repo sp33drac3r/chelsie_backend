995f3589fc91d87ba72ef26624051c800aa24c70
Elizabeth Brown <email": null>
2016-06-05 17:06:05 -0700

    Add action methods to posts controller.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,56 @@
 class PostsController < ApplicationController
-  def index
-    @school = Center.all
-    render :json => @centers
-  end
+  # def index
+  #   @school = School.find(params[:school_id])
+  #   @posts = @school.posts
+
+  #   render :json => {
+  #     :school => @school,
+  #     :posts => @posts
+  #   }
+  # end
 
   def show
-    @center = Center.find(params[:id])
-    render :json => @center
+    @post = Post.find(params[:id])
+    @comments = @post.comments
+
+    render :json => {
+      :post => @post,
+      :comments => @comments
+    }
   end
+
+  def create
+    @post = Post.new(post_params)
+    @post.school_id = params[:school_id]
+
+    if @post.save
+      render :json => @post
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+
+    render :json => @post
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      render :json => @post
+    end
+  end
+
+  def delete
+    @post = Post.find(params[:id])
+    @post.destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :user_id)
+  end
+
 end
