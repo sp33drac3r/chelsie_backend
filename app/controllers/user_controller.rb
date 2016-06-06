35fec6c87f383402683decbe0ce5dbd7c40d36a8
Riley Scheid <riley.k.scheid@gmail.com>
2016-06-06 11:05:25 -0700

    Add create update and delete to users_controller

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,33 @@
+class UserController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render :json => @user
+    else
+      render :json => {response: 'Failed to save User'}
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      render :json => @user
+    else
+      render :json => {response: 'Failed to update'}
+    end
+  end
+
+  def destroy
+    @user = User.find_by(params[:id])
+    if @user.destroy
+      render :json => {response: 'User deleted'}
+    else
+      render :json => {response: 'Failed to delete User'}
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+end
