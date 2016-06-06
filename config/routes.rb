0e76599f50ba49410930a498776cbab9be877623
Riley Scheid <riley.k.scheid@gmail.com>
2016-06-06 11:07:08 -0700

    Pluralize routes for users/

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'user/create'
+  get 'users/create'
 
-  get 'user/update'
+  get 'users/update'
 
-  get 'user/destroy'
+  get 'users/destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
