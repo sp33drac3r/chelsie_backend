4a7e46baae147f183e0f3483d672d0750fa56473
Riley Scheid <riley.k.scheid@gmail.com>
2016-06-06 17:27:50 -0700

    Reverse post order

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,4 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find_by(id: params[:id])
-    if @user
-      render :json => @user
-    else
-      render :json => {response: 'You done fucked up'}
-    end
-  end
-
   def create
     @user = User.new(user_params)
     if @user.save
